4d4b20b0b60558f2fab3761ccceb903230cfef67
Jonathan Knowles <mail@jonathanknowles.net>
2016-08-12 17:59:59 +0800

    Adding simple executable tests.

--- executable/Main.hs
+++ executable/Main.hs
@@ -16,51 +16,41 @@ monitor = terminalMonitor 0.5
 
 testHashTree :: IO ()
 testHashTree = do
-    putStrLn "hashing file trees"
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashTree "/public/jsk/scratch/empty")
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashTree "/public/jsk/scratch/small")
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashTree "/public/jsk/scratch/small2")
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashTree "/public/jsk/scratch/large")
+    putStrLn "hashing directory trees"
+    Prelude.print =<< runSafeMonitoredEffect monitor (hashTree "/path/to/example-directory-1")
+    Prelude.print =<< runSafeMonitoredEffect monitor (hashTree "/path/to/example-directory-1")
 
 testHashFile :: IO ()
 testHashFile = do
     putStrLn "Hashing files"
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashFile "/public/jsk/scratch/small/1MiB")
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashFile "/public/jsk/scratch/small/4MiB")
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashFile "/public/jsk/scratch/small/16MiB")
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashFile "/public/jsk/scratch/small/64MiB")
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashFile "/public/jsk/scratch/large/256MiB")
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashFile "/public/jsk/scratch/large/512MiB")
-    Prelude.print =<< runSafeMonitoredEffect monitor (hashFile "/public/jsk/scratch/large/1GiB")
+    Prelude.print =<< runSafeMonitoredEffect monitor (hashFile "/path/to/example-file-1")
+    Prelude.print =<< runSafeMonitoredEffect monitor (hashFile "/path/to/example-file-2")
 
 testCopyFile :: IO ()
 testCopyFile = do
     putStrLn "Copying files"
-    runSafeMonitoredEffect monitor (copyFile "/public/jsk/scratch/small/1MiB" "/dev/null")
-    runSafeMonitoredEffect monitor (copyFile "/public/jsk/scratch/small/1MiB" "/dev/null")
-    runSafeMonitoredEffect monitor (copyFile "/public/jsk/scratch/small/4MiB" "/dev/null")
-    runSafeMonitoredEffect monitor (copyFile "/public/jsk/scratch/small/16MiB" "/dev/null")
-    runSafeMonitoredEffect monitor (copyFile "/public/jsk/scratch/small/64MiB" "/dev/null")
-    runSafeMonitoredEffect monitor (copyFile "/public/jsk/scratch/large/256MiB" "/dev/null")
-    runSafeMonitoredEffect monitor (copyFile "/public/jsk/scratch/large/512MiB" "/dev/null")
-    runSafeMonitoredEffect monitor (copyFile "/public/jsk/scratch/large/1GiB" "/dev/null")
+    runSafeMonitoredEffect monitor (copyFile "/path/to/example-file-1" "/dev/null")
+    runSafeMonitoredEffect monitor (copyFile "/path/to/example-file-2" "/dev/null")
 
 testCopyTree :: IO ()
-testCopyTree =
-    runSafeMonitoredEffect monitor (copyTree "/public/jsk/scratch/small" "/public/jsk/scratch/small2")
+testCopyTree = do
+    putStrLn "Copying directory trees"
+    runSafeMonitoredEffect monitor (copyTree "/path/to/example-directory-1" "/path/to/example-directory-3")
+    runSafeMonitoredEffect monitor (copyTree "/path/to/example-directory-2" "/path/to/example-directory-4")
 
 testCalculateDiskUsage :: IO ()
 testCalculateDiskUsage = do
     putStrLn "Calculating disk usage"
-    _ <- runSafeMonitoredEffect monitor $ calculateDiskUsage "/public"
+    _ <- runSafeMonitoredEffect monitor $ calculateDiskUsage "/path/to/example-directory-1"
+    _ <- runSafeMonitoredEffect monitor $ calculateDiskUsage "/path/to/example-directory-2"
     return ()
 
 main :: IO ()
 main = do
     S.hSetBuffering S.stdout S.NoBuffering
+    putStrLn "Please edit the file 'executable/Main.hs' to run the example code."
     --testCopyTree
     --testHashTree
     --testCalculateDiskUsage
-    testHashFile
-    testCopyFile
-    --
+    --testHashFile
+    --testCopyFile
